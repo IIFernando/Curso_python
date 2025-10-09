3849dc8e5211525e862eaf3d0b0ddc3b490efe3d
Fernando <fernandoaraujocontato@outlook.com>
2025-10-09 20:26:10 -0300

    Atualização de pastas

--- Aulas Udemy/Enumerate.py
+++
@@ -1,9 +0,0 @@
-"""
- Enumerate - enumera iteráveis (índices)
-"""
-
-lista = ['Uva', 'Manga', ' Limão']
-lista.append('Banana')
-
-for indice, valor in enumerate(lista):
-    print(indice, valor)
--- Aulas Udemy/OperacaoTernaria.py
+++
@@ -1,7 +0,0 @@
-"""
- Operação ternária (condicional de uma linha)
- <valor> IF <condição> else <outro valor>
-"""
-
-condicao = 10 == 10
-print('valor' if condicao else 'outro valor')
